0ffa4e3336cd3151e1a6be8c5e6974afd73d51e4
Elena <elena.guzbla@gmail.com>
2015-12-30 16:34:49 +0100

    Update MenuIU.cs

--- MenuIU.cs
+++ MenuIU.cs
@@ -3,7 +3,8 @@
 /// </summary>
 static void IUMenu()
         {
-            while (opcion != 3)
+                int opcion = 0;
+            while (opcion != 5)
             {
             	Console.WriteLine("1. Opción 1");
             	Console.WriteLine("2. Opción 2");
@@ -12,19 +13,33 @@ static void IUMenu()
             	Console.WriteLine("5. Salir");
             
             	// Elegir opción para el menú
-              opcion = ObtenerNum(0, 6);
+              opcion = ObtenerNum(1, 6);
               Console.WriteLine("Has elegido la opción " + opcion);
-              Console.ReadLine();
               Console.Clear();
+                // Si la opción no es de salida...
+                if (opcion != 5)
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                                // Método 1
+                            break;
+                        case 2:
+                                // Método 2
+                            break;
+                        case 3:
+                                // Método 3
+                            break;
+                        case 4:
+                                //Método 4
+                            break;
+                    }
+                    Console.ReadLine();
+                }
+                Console.Clear();
 
-            	 // Si la opción no es de salida...
-            	 if (opcion != 5)
-            	 {
-            	     GestionOp(opcion);
-            	 }
-             
-             }
-             
+            }
+            // Salida del menú
             Console.WriteLine("Saliendo del programa...");
             Console.ReadLine();
         }
@@ -49,7 +64,7 @@ static void IUMenu()
             {
                 Console.WriteLine("Número no válido");
             }
-            while (num <= min || num >= max)
+            while (num < min || num >= max)
             {
                 Console.Write("Número no válido, ha de ser entre {0} y {1}: ", min, max);
                 try
