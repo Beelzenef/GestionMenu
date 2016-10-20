eaaddebbcaf07623426e9378c491f16ecb477f8f
Elena <elena.guzbla@gmail.com>
2016-10-20 19:46:29 +0200

    Plantilla para CS

---
+++ Base.cs
@@ -0,0 +1,31 @@
+﻿/*
+    PROGRAMA: 
+    AUTOR: 
+    FECHA: /Oct/2016
+    DESCRIPCION: Este programa 
+*/
+
+using System;
+
+namespace AppC
+{
+    class AppC
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+
+            // Titulo de consola
+            Console.Title = "";
+
+            Console.WriteLine("Este programa \n");
+
+            
+
+            // Fin de programa
+            Console.WriteLine("\n\nPulsa ENTER para salir...");
+            Console.ReadLine();
+        }
+
+      }
+}
