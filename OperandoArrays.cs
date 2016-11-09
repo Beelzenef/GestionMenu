7705973ee63b834fc2ce54476c9e44b43f05862e
Elena <elena.guzbla@gmail.com>
2016-11-09 12:12:16 +0100

    Parametros de Main y operando arrays

---
+++ OperandoArrays.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace OperandoArrays
+{
+    class OperandoArrays
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int[] listilla = { };
+            string[] listillaPalabras = {"C#", "Python", "SASS", "Javascript" };
+
+            // Titulo de consola
+            Console.Title = Operando arrays";
+
+            Console.WriteLine("Utilizando listas, arrays");
+            listilla = LlenarDatos();
+            ListarDatos(listilla);
+            Console.WriteLine();
+            ListarDatos(listillaPalabras);
+
+            // Fin de programa
+            Console.WriteLine("\n\nPulsa ENTER para salir...");
+            Console.ReadLine();
+        }
+
+        static void ListarDatos(int[] datos)
+        {
+            for (int i = 0; i < datos.Length; i++)
+                Console.Write("{0}\t", datos[i]);
+        }
+
+        static void ListarDatos(string[] datos)
+        {
+            for (int i = 0; i < datos.Length; i++)
+                Console.Write("{0}\t", datos[i]);
+        }
+
+        static int[] LlenarDatos()
+        {
+            int[] datos = new int[5];
+
+            for (int i = 0; i < datos.Length; i++)
+                datos[i] = i + 1;
+
+            return datos;
+        }
+    }
+}
---
+++ ParametrosMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ParametrosMain
+{
+    class ParametrosMain
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int numeroRecibido = 0;
+
+            // Titulo de consola
+            Console.Title = "Visitando parametros de Main";
+
+            // Simulando llamada de aplicacion:
+                    // C:> nombreAplicacion texto numero
+            // Mostrando el texto las veces que indique el numero
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Faltan parámetros");
+                Console.ReadLine();
+                return;
+            }
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Demasiados parámetros");
+                Console.ReadLine();
+                return;
+            }
+
+            try
+            {
+                numeroRecibido = int.Parse(args[1]);
+                for (int i = 0; i < numeroRecibido; i++)
+                    Console.WriteLine(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("El segundo parámetro no es un numero entero...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
