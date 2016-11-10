093da2d7237ce3a780c3d62e74f116313f1541a5
Elena <elena.guzbla@gmail.com>
2016-11-10 11:08:47 +0100

    Mejora, PassNoEcho()

--- ejemplosClase/loginUsuario.cs
+++ ejemplosClase/loginUsuario.cs
@@ -15,23 +15,58 @@ namespace Identificate
         static void LoginUsuario()
         {
             string user = string.Empty;
-            string passw = string.Empty;
-            ConsoleKeyInfo tecla;
+            string passUser = string.Empty;
 
             Console.WriteLine("¿Cual es tu usuario?");
             user = Console.ReadLine();
             Console.WriteLine("¿Contraseña?");
-
-            do
-            {
-                tecla = Console.ReadKey(true);
-                Console.Write("*");
-                passw += tecla.KeyChar;
-            } while (tecla.Key != ConsoleKey.Enter);
+            passUser = PassNoEcho();
 
             Console.WriteLine();
             Console.WriteLine("Usuario {0}, tu contraseña es {1}", user, passw);
             Console.WriteLine("Tu contraseña es de longitud {0}, ¡poco segura!", passw.Length);
+        }
+        
+         static void PedirPass()
+        {
+            string passCorrecta = "LOL";
+            string passIntento = string.Empty;
+            bool noValida = true;
+            int nIntentos = 0;
+
+            while (noValida && nIntentos < 3)
+            {
+                Console.WriteLine("\n¿Cual es la contraseña?");
+                passIntento = PassNoEcho();
+
+                if (passIntento == passCorrecta)
+                    noValida = !noValida;
+                nIntentos++;
+            }
+
+            if (!noValida)
+                Console.WriteLine("\nHas acertado");
+            else
+                Console.WriteLine("\nFallaste");
+
+        }
+        
+        static string PassNoEcho()
+        {
+            string passw = string.Empty;
+            ConsoleKeyInfo tecla;
+
+            do
+            {
+                tecla = Console.ReadKey(true);
+                if (Char.IsLetterOrDigit(tecla.KeyChar))
+                {
+                    passw += tecla.KeyChar;
+                    Console.Write("*");
+                }
+            } while (tecla.Key != ConsoleKey.Enter);
+
+            return passw;
         }
     }
 }
