4f23365f4e07ba4c2214763051e6cf48f3e5db27
Elena <elena.guzbla@gmail.com>
2016-11-28 10:41:13 +0100

    Busquedas y reflexiones sobre arrays

---
+++ ejemplosClase/BusquedaArrays.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace BusquedaArrays
+{
+	class BusquedaArrays
+	{
+		static void Main(string[] args)
+		{
+			// Variables
+			int[] vector = { 1, 2, 5, 6, 7, 8, 11 };
+			int datoEncontrado = 0;
+			int[] nuevoVector;
+
+			// Titulo de consola
+			Console.Title = "Algoritmos de busqueda en arrays (pt1)";
+
+			Console.WriteLine ("Busqueda secuencial de un dato, {0}", 5);
+			datoEncontrado = BusquedaSec (5, vector);
+
+			if (datoEncontrado != -1)
+				Console.WriteLine ("El dato buscado en la pos. {0} y vale {1}", datoEncontrado + 1, vector[datoEncontrado]);
+			else
+				Console.WriteLine ("El dato no se encuentra en el array");
+
+			Console.WriteLine ("\n\nBusqueda binaria de un dato, {0}", 5);
+			datoEncontrado = BusquedaBin (5, vector);
+
+			if (datoEncontrado != -1)
+				Console.WriteLine ("El dato buscado en la pos. {0} y vale {1}", datoEncontrado + 1, vector[datoEncontrado]);
+			else
+				Console.WriteLine ("El dato no se encuentra en el array");
+
+			try {
+				nuevoVector = CrearArray (10000);
+				Console.WriteLine ("\n\nBusqueda binaria de un dato, {0}", 1678);
+				datoEncontrado = BusquedaBin (1678, nuevoVector);
+
+				if (datoEncontrado != -1)
+					Console.WriteLine ("El dato buscado en la pos. {0} y vale {1}", datoEncontrado + 1, nuevoVector[datoEncontrado]);
+				else
+					Console.WriteLine ("El dato no se encuentra en el array");
+			}
+			catch {
+				Console.WriteLine ("Error rellenando array");
+			}
+
+			// Salida
+			Console.WriteLine ("\n\nPulsa ENTER para salir...");
+			Console.ReadLine ();
+
+		}
+
+		static int[] CrearArray(int longitud)
+		{
+			int[] vectorNuevo = new int[longitud];
+
+			for (int i = 0; i < longitud; i++)
+				vectorNuevo [i] = i + 1;
+
+			return vectorNuevo;
+		}
+
+		static void RecorrerArray(int[] unArray)
+		{
+			for (int i = 0; i < unArray.Length; i++) {
+				Console.Write ("{0} ", unArray [i]);
+			}
+		}
+
+		/// <summary>
+		/// Realiza busqueda secuencial del dato en el array "vector"
+		/// </summary>
+		/// <returns>Indice del dato a buscar, -1 si no lo encuentra</returns>
+		/// <param name="datoABuscar">Dato a buscar.</param>
+		/// <param name="vector">Nombre del array </param>
+		/// <param name="nDatos">Numero de datos entre los que buscar</param>
+		static int BusquedaSec(int datoABuscar, int[] vector, int nDatos)
+		{
+			for (int i = 0; i < nDatos; i++)
+				if (datoABuscar == vector[i])
+					return i;
+			return -1;    
+		}
+
+		static int BusquedaSec(int datoABuscar, int[] vector)
+		{
+			for (int i = 0; i < vector.Length; i++)
+				if (datoABuscar == vector[i])
+					return i;
+			return -1;    
+		}
+
+		static int BusquedaBin(int datoABuscar, int[] vector)
+		{
+			int posIni = 0;
+			int tamanio = vector.Length;
+			int posFinal = tamanio - 1;
+			int posMedio = (posIni + posFinal) / 2;;
+
+			while (vector [posMedio] != datoABuscar && posIni <= posFinal) {
+				posMedio = (posIni + posFinal) / 2;
+				if (datoABuscar > vector [posMedio])
+					posIni = posMedio + 1;
+				else
+					posFinal = posMedio - 1;
+			}
+			// ¿Ha encontrado el dato?
+			if (vector [posMedio] == datoABuscar)
+				return posMedio;
+			return -1;
+		}
+
+	}
+}
+
---
+++ ejemplosClase/InfoArrays.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace InfoArrays
+{
+	class InfoArrays
+	{
+		static void Main(string[] args)
+		{
+			// Variables
+			int[] vector = { 1, 2, 5, 6, 7, 8, 11 };
+			char[,] vector2D = { { 'a', 'b', 'c' },
+								{ 'x', 'y', 'z' },
+								{ 'h', 'b', 'o' } };
+
+			// Titulo de consola
+			Console.Title = "Informacion de arrays";
+
+			Console.WriteLine ("Informacion de array:");
+			InfoArray (vector);
+			Console.WriteLine ("Recorriendo array:");
+			RecorrerArray (vector);
+
+			Console.WriteLine ("\n\nInformacion de array de dos dimensiones:");
+			InfoArray (vector2D);
+
+			// Salida
+			Console.WriteLine ("\n\nPulsa ENTER para salir...");
+			Console.ReadLine ();
+
+		}
+
+		static void RecorrerArray(int[] unArray)
+		{
+			for (int i = 0; i < unArray.Length; i++) {
+				Console.Write ("{0} ", unArray [i]);
+			}
+		}
+
+		static void InfoArray(Array unArray)
+		{
+			if (unArray is Array) {
+				Console.WriteLine ("- - D A T O S  D E  U N  A R R A Y - -");
+				Console.WriteLine ("--------------------------------------");
+				Console.WriteLine ("Tipo: \t\t\t{0}", unArray.GetType ());
+				Console.WriteLine ("¿Es solo lectura? \t{0}", unArray.IsReadOnly);
+				Console.WriteLine ("Longitud: \t\t{0}", unArray.Length);
+				Console.WriteLine ("Rank: \t\t\t{0}", unArray.Rank);
+				Console.WriteLine ("Indice menor: \t\t{0}", unArray.GetLowerBound (0));
+				Console.WriteLine ("Tamaño fijo: \t\t{0}", unArray.IsFixedSize);
+			}
+			
+		}
+	}
+}
+
