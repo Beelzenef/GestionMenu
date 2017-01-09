29e6049ddc7d60f6d996fab5c9dfc1b5db9f9ff9
Elena <elena.guzbla@gmail.com>
2017-01-09 10:34:02 +0100

    Ordenacion de cartas

---
+++ ordCartas/Carta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace egb.ordenandoCartas
+{
+	public class Carta : IComparable<Carta>
+	{
+		// Campos
+		public int valor;
+		public string imagen;
+		public Palos palo;
+
+		// Propiedades
+		public int Valor {
+			get { return this.valor; }
+			set { this.valor = value; }
+		}
+
+		public string Imagen {
+			get { return this.imagen; }
+			set { this.imagen = value; }
+		}
+
+		public Palos Palo {
+			get { return this.palo; }
+			set { this.palo = value; }
+		}
+
+		// Enumeracion
+		public enum Palos {
+			Oro, Copas, Espadas, Bastos
+		}
+
+		// Metodos
+		public int CompareTo(Carta o)
+		{
+			int comparador = this.palo.CompareTo (o.palo);
+
+			if (comparador != 0)
+				return comparador;
+			comparador = this.valor.CompareTo (o.valor);
+			return comparador;
+		}
+
+		// Constructores
+		public Carta (Palos p, string i, int v)
+		{
+			this.palo = p;
+			this.imagen = i;
+			this.valor = v;
+		}
+
+		public Carta (Palos p, int v)
+		{
+			this.palo = p;
+			this.valor = v;
+		}
+	}
+}
+
---
+++ ordCartas/OrdCartas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace egb.ordenandoCartas
+{
+	class ordenandoCartas
+	{
+		static List<Carta> Baraja = new List<Carta> (48);
+
+		static void ListarBaraja(List<Carta> b)
+		{
+			foreach (Carta item in b) {
+				Console.WriteLine ("Carta: {0,2}\t{1}", item.Valor, item.Palo);
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.Title = "Operando con diccionarios";
+
+			Baraja.Add (new Carta (Carta.Palos.Bastos, 1));
+			Baraja.Add (new Carta (Carta.Palos.Espadas, 6));
+			Baraja.Add (new Carta (Carta.Palos.Oro, 12));
+			Baraja.Add (new Carta (Carta.Palos.Oro, 2));
+			Baraja.Add (new Carta (Carta.Palos.Copas, 4));
+			Baraja.Add (new Carta (Carta.Palos.Espadas, 2));
+			Baraja.Add (new Carta (Carta.Palos.Espadas, 5));
+
+			Console.WriteLine ("\nListado de la baraja");
+			ListarBaraja (Baraja);
+
+			Console.WriteLine ("\nListado de la baraja ordenada");
+			Baraja.Sort ();
+			ListarBaraja (Baraja);
+
+			Console.ReadLine ();
+		}
+	}
+}
