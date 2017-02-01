6789d9db6253a2d0d64fffbf61a073a43e214b67
Elena G <elena.guzbla@gmail.com>
2017-02-01 11:58:21 +0100

    Polimorfismo con interfaces

---
+++ ejemplosClase/MasInterfaces.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace egb.Interfaces
+{
+	interface ICopias
+	{
+		void Copiar();
+	}
+
+	class Pila : ICopias
+	{
+		public void Clonar()
+		{
+			Console.WriteLine ("Soy Clonar() de PILA");
+		}
+
+		public void Copiar()
+		{
+			Console.WriteLine ("Soy Copiar() de PILA");
+		}
+	}
+
+	class Factura : ICopias
+	{
+		public void Clonar()
+		{
+			Console.WriteLine ("Soy Clonar() de FACTURA");
+		}
+
+		public void Copiar()
+		{
+			Console.WriteLine ("Soy Copiar() de FACTURA");
+		}
+	}
+
+	class Inicio
+	{
+		static void Main()
+		{
+			/*
+			Pila unaPila = new Pila ();
+			Factura unaFac = new Factura ();
+
+			unaFac.Clonar ();
+			unaFac.Copiar ();
+
+			unaPila.Clonar ();
+			unaPila.Copiar ();
+			*/
+
+			ICopias pila = new Pila ();
+			ICopias factura = new Factura ();
+
+			pila.Copiar ();
+			factura.Copiar ();
+
+			Console.ReadLine ();
+		}
+	}
+
+}
