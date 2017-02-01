7e0989f24382f37da2f94a4c0f0e757afe4c2821
Elena G <elena.guzbla@gmail.com>
2017-02-01 10:51:39 +0100

    Ejemplo interface

---
+++ ejemplosClase/UsoInterfaces.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace egb.Interfaces
+{
+	public delegate void Anotacion (string s);
+	interface IPrueba
+	{
+		string Anotacion { get; set; }
+		void Mostrar();
+		string this [int index] { get; }
+		event Anotacion Anadida;
+	}
+
+	class Agenda : OtraClase, IPrueba
+	{
+		string nota;
+
+		public string Anotacion { get { return nota; } set { nota = value; } }
+
+		public void Mostrar()
+		{
+			Console.WriteLine ("LOL");
+		}
+		public string this [int index] { get { throw new NotImplementedException (); } }
+
+		public event Anotacion Anadida;
+	}
+
+	class OtraClase
+	{
+		public void Ver()
+		{
+
+		}
+	}
+
+	class Inicio
+	{
+		static void Main()
+		{
+			Console.ReadLine ();
+		}
+	}
+
+}
