6945ec4621111d23a3e69d6c4faf7de1411b6c1d
JiV84 <Spunish@yahoo.es>
2017-03-29 08:40:46 +0200

    Reorganizacion de repo

---
+++ P1_pruebaFicheros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PruebasFicheros
+{
+    public class P1
+    {
+        public static void CreaMergeListas()
+        {
+            ArrayList lista1 = new ArrayList();
+            ArrayList lista2 = new ArrayList();
+
+            lista1.Add(1);
+            lista1.Add("Hola");
+
+            lista2.Add("Elemento en lista 2");
+            lista2.AddRange(lista1);
+
+            List<string> listilla = new List<string>();
+            listilla.Add("Lista");
+
+            lista2.AddRange(listilla);
+
+            Mostrar(lista2);
+
+        }
+
+        public static void Mostrar(ArrayList ls)
+        {
+            foreach (object item in ls)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
