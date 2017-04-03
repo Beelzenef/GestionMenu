dd40f116ded503ea2103ce4434bc75a291571c4a
Elena G <elena.guzbla@gmail.com>
2017-04-03 11:01:59 +0200

    combo y list boxes

---
+++ ejemplosClase/wpfing/selectores/selectoresZZ.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppW_Selectores
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboB_Juegos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Title = ((TextBlock)comboB_Juegos.Items[comboB_Juegos.SelectedIndex]).Text;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Title = lsB_NJugadores.SelectedIndex.ToString();
+        }
+    }
+}
