9c507ab720cf38d4754b45a1b710371fd2e917cd
Elena G <elena.guzbla@gmail.com>
2017-04-24 11:53:10 +0200

    comandos de cierre y binding

---
+++ ejemplosClase/wpfing/cierreEnComando/cierreCommand.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CerrandoWin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (chkB_puedeCerrar != null)
+                e.CanExecute = (bool)chkB_puedeCerrar.IsChecked;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
