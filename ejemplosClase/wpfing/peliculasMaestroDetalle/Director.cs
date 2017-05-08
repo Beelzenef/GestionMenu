81ece1de4ddf80a8a261b55c0cf4518a0a0a8bc9
Elena G <elena.guzbla@gmail.com>
2017-05-08 09:50:45 +0200

    Maestro Detalle con Peliculas

---
+++ ejemplosClase/wpfing/peliculasMaestroDetalle/Director.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WPF_Peliculas
+{
+    public class Director : Notificador
+    {
+        ObservableCollection<Pelicula> listita = new ObservableCollection<Pelicula>();
+
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        public string Nacionalidad
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Pelicula> Filmes
+        {
+            get { return listita; }
+            set { listita = value; }
+        }
+
+        #region Constructores
+
+        public Director()
+        {
+        }
+
+        #endregion
+    }
+}
