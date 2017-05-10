9b479799902b9f8c1072b1ad3c192af6b0301e2c
Elena G <elena.guzbla@gmail.com>
2017-05-10 12:08:18 +0200

    binging ListView - WPF

---
+++ ejemplosClase/wpfing/AppWPF_ListView/AppWPF_ListView/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+
+namespace AppWPF_ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListaPersonas listitaPersonas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listitaPersonas = new ListaPersonas();
+            lsV_Jugadores.ItemsSource = listitaPersonas;
+        }
+
+        private void btn_AnadirDatos_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtB_Nombre.Text.Length == 0 || txtB_NombrePJ.Text.Length == 0)
+                return;
+
+            try
+            {
+                listitaPersonas.Add(new Persona(txtB_Nombre.Text, txtB_NombrePJ.Text, int.Parse(txtB_NivelPJ.Text)));
+                LimpiarTxtBoxes();
+            }
+            catch
+            {
+                MessageBox.Show("Persona imposible de añadir");
+            }
+        }
+
+        private void btn_EliminarDatos_Click(object sender, RoutedEventArgs e)
+        {
+            if (lsV_Jugadores.SelectedIndex == -1)
+                return;
+
+            listitaPersonas.RemoveAt(lsV_Jugadores.SelectedIndex);
+        }
+
+        private void btn_CambiarDatos_Click(object sender, RoutedEventArgs e)
+        {
+            Persona pTMP = lsV_Jugadores.SelectedItem as Persona;
+
+            if (pTMP == null)
+                return;
+            int posicionP = lsV_Jugadores.SelectedIndex;
+            CambiarNivel(pTMP);
+            listitaPersonas.RemoveAt(posicionP);
+            listitaPersonas.Insert(posicionP, pTMP);
+        }
+
+        private void CambiarNivel(Persona p)
+        {
+            try
+            {
+                p.Nivel = int.Parse(txtB_NivelPJ.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nuevos datos incompatibles");
+            }
+        }
+
+        private void LimpiarTxtBoxes()
+        {
+            txtB_Nombre.Clear();
+            txtB_NombrePJ.Clear();
+            txtB_NivelPJ.Clear();
+
+            txtB_Nombre.Focus();
+        }
+    }
+}
---
+++ ejemplosClase/wpfing/AppWPF_ListView/AppWPF_ListView/Persona.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace AppWPF_ListView
+{
+    public class Persona : INotifyPropertyChanged
+    {
+        string nombre;
+        string personaje;
+        int nivel;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+                OnPropertyChanged("Nombre");
+            }
+        }
+
+        public string Personaje
+        {
+            get { return personaje; }
+            set
+            {
+                personaje = value;
+                OnPropertyChanged("Personaje");
+            }
+        }
+
+        public int Nivel
+        {
+            get { return nivel; }
+            set
+            {
+                nivel = value;
+                OnPropertyChanged("Nivel");
+            }
+        }
+
+        public Persona(string n, string p, int l)
+        {
+            Nivel = l;
+            Nombre = n;
+            Personaje = p;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string nombre)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nombre));
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+
+    public class ListaPersonas : ObservableCollection<Persona>
+    {
+        ObservableCollection<Persona> listaPersonas = new ObservableCollection<Persona>();
+
+        public ListaPersonas() : base()
+        {
+            Add(new Persona("Elena", "Ethan Thross", 20));
+            Add(new Persona("Irene", "Catherin Thross", 20));
+            Add(new Persona("Jaime", "Lord Gideon", 20));
+        }
+
+    }
+}
